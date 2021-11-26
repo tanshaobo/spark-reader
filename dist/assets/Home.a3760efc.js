82911cc6442b9b59208d4837b6b981a68a2c37ab
tanshaobo <18235140339@163.com>
2021-11-26 17:55:10 +0800

    初始化

---
+++ dist/assets/Home.a3760efc.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index.5df2b2cc.js";import{o as r,b as o}from"./vendor.29b45b4f.js";const c={};function n(t,s){return r(),o("div")}var _=e(c,[["render",n]]);export{_ as default};
---
+++ src/views/Home.vue
@@ -0,0 +1,6 @@
+<template>
+  <div></div>
+</template>
+
+<script setup></script>
+<style lang="stylus" scoped></style>
