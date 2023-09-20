0c3b585ee7a940c22c8e0afad763082960da4094
tanshaobo <18235140339@163.com>
2023-09-20 15:07:31 +0800

    fix(components): 删除面包屑导航中关于window.history中的关联项

--- src/components/layout/Header/index.vue
+++ src/components/layout/Header/index.vue
@@ -2,7 +2,7 @@
  * @Author: tanshaobo
  * @Date: 2023-06-17 10:47:00
  * @LastEditors: tanshaobo
- * @LastEditTime: 2023-09-07 17:08:44
+ * @LastEditTime: 2023-09-20 15:02:45
  * @Description: file content
  * @FilePath: \spark-reader\src\components\layout\Header\index.vue
 -->
@@ -11,11 +11,8 @@
     <el-breadcrumb>
       <template v-for="(item, index) in breadcrumbList" :key="index">
         <el-breadcrumb-item
-          v-if="index != breadcrumbList.length"
-          :to="{
-            name: item.name,
-            state: item.params
-          }"
+          class="is-link"
+          v-if="index !== breadcrumbList.length - 1"
           @click="goRoute(item)"
         >
           {{ item.label }}
@@ -35,22 +32,36 @@ import { useRoute, useRouter } from 'vue-router'
 const route = useRoute()
 const router = useRouter()
 
-const breadcrumbList = computed(() => {
-  const { meta } = route
-  const { params } = window.history.state
+const state = reactive({
+  breadcrumbList: []
+})
+
+const setBreadcrumbList = () => {
+  const { meta, query } = route
   let tempArr = []
   if (meta && meta.crumb) {
     tempArr = [...meta.crumb]
   }
-  tempArr[tempArr.length - 1].params = params
+  tempArr[tempArr.length - 1].query = query
+  console.log('route', route)
+  console.log('tempArr', tempArr)
+}
+setBreadcrumbList()
+const breadcrumbList = computed(() => {
+  const { meta, query } = route
+  let tempArr = []
+  if (meta && meta.crumb) {
+    tempArr = [...meta.crumb]
+  }
+  tempArr[tempArr.length - 1].query = query
   return [...tempArr]
 })
 
 const goRoute = (item) => {
   router.push({
     name: item.name,
-    state: {
-      ...item.params
+    query: {
+      ...item.query
     }
   })
 }
@@ -62,4 +73,13 @@ const goRoute = (item) => {
   .el-breadcrumb
     line-height 60px
     height 60px
+    .is-link
+      :deep(.el-breadcrumb__inner)
+        font-weight 700
+        text-decoration none
+        transition color .2s cubic-bezier(.645,.045,.355,1)
+        color #303133
+        &:hover
+          color #409eff
+          cursor pointer
 </style>
