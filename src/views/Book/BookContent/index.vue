d494be094a373ddb59c56c9f54fab49b2e09eb3e
tanshaobo <18235140339@163.com>
2023-09-20 18:26:11 +0800

    fix(components): 字段变更

--- src/views/Book/BookContent/index.vue
+++ src/views/Book/BookContent/index.vue
@@ -2,7 +2,7 @@
  * @Author: tanshaobo
  * @Date: 2023-08-18 14:55:58
  * @LastEditors: tanshaobo
- * @LastEditTime: 2023-09-12 20:02:19
+ * @LastEditTime: 2023-09-20 17:23:47
  * @Description: 书籍内容页
  * @FilePath: \spark-reader\src\views\Book\BookContent\index.vue
 -->
@@ -22,10 +22,11 @@ const state = reactive({
 
 const route = useRoute()
 
-const { query } = route
+const { params } = route
 
 const GetContent = () => {
-  getContent(query).then((res) => {
+  console.log(params)
+  getContent(params).then((res) => {
     state.content = res
   })
 }
