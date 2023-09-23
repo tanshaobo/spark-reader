c067e978d415fb808345c32115602567bf623a35
tanshaobo <18235140339@163.com>
2023-09-23 14:41:19 +0800

    build(deps): 删除vuex,增加测试数据

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@
  * @Author: tanshaobo
  * @Date: 2023-06-08 15:34:10
  * @LastEditors: tanshaobo
- * @LastEditTime: 2023-08-18 16:18:06
+ * @LastEditTime: 2023-09-23 14:40:01
  * @Description: 入口文件部分
  * @FilePath: \spark-reader\src\main.js
  */
@@ -10,12 +10,10 @@ import { createApp } from 'vue'
 import router from '@/router'
 import ElementPlus from 'element-plus'
 import App from './App.vue'
-import store from './store'
 import './style/common.css'
 import 'element-plus/dist/index.css'
 
 const app = createApp(App)
 app.use(router)
-app.use(store)
 app.use(ElementPlus)
 app.mount('#app')
